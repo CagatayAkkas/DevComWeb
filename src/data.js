68ac6c71b52057cfa01b6956ce1159be4f09d0bf
Cagatay Akkas <cagatayakkas9191@gmail.com>
2023-07-04 20:20:33 +0300

    first

---
+++ src/data.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { FaBehance, FaDiscord, FaLinkedin, FaTwitter } from "react-icons/fa";
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "Yol Haritaları",
+  },
+  {
+    id: 2,
+    url: "/LeetCode",
+    text: "LeetCode",
+  },
+  {
+    id: 3,
+    url: "/Ethernaut",
+    text: "Ethernaut",
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: "https://discord.gg/PreBDJg48r",
+    icon: <FaDiscord />,
+  },
+  {
+    id: 2,
+    url: "https://twitter.com/_DevCom",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.linkedin.com/company/trdevelopercommunity/",
+    icon: <FaLinkedin />,
+  },
+];
