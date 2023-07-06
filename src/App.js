cbbfd23313dd2e2b631b3c4c02d5600d15c0b016
Cagatay Akkas <cagatayakkas9191@gmail.com>
2023-07-06 20:21:19 +0300

    menu added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <>
-      <Navbar></Navbar>
+      <main>
+        <Navbar></Navbar>
+        <section className="menu section">
+          <div className="title">
+            <h2>our menu</h2>
+            <div className="underline"></div>
+          </div>
+          <Categories categories={categories} filterItems={filterItems} />
+          <Menu items={menuItems} />
+        </section>
+      </main>
     </>
   );
 }
