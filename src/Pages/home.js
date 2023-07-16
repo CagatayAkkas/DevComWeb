b8e325370e1da70e31cb48bedf6d58d90302af1f
Cagatay Akkas <cagatayakkas9191@gmail.com>
2023-07-16 21:20:11 +0300

    home page added fav icon changed

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,17 +1,19 @@
 import React from "react";
+import banner from "../images/banner.jpg";
+import banner1 from "../images/banner1.jpg";
 
-function home() {
+function Home() {
   const handleDiscordClick = () => {
     // Handle the click event for the Discord link
     // For example, open the Discord link in a new tab
-    window.open("https://discord.com/your-discord-link");
+    window.open("https://discord.gg/PreBDJg48r");
   };
 
   return (
     <section className="home">
       <div className="background-image">
         {/* Replace the 'image-url' with the URL of your big image */}
-        <img src="image-url" alt="Background" />
+        <img src={banner1} alt="Background" />
 
         <div className="image-overlay">
           {/* Replace 'Your Text' with the desired text for the overlay */}
@@ -20,7 +22,7 @@ function home() {
 
           {/* Replace 'image-url' with the URL of your clickable image */}
           <img
-            src="image-url"
+            src={banner}
             alt="Clickable Image"
             onClick={handleDiscordClick}
           />
@@ -28,9 +30,7 @@ function home() {
           {/* Replace 'Your Discord Link' with the desired text for the Discord link */}
           <p>
             Join our Discord community:{" "}
-            <a href="https://discord.com/your-discord-link">
-              Your Discord Link
-            </a>
+            <a href="https://discord.gg/PreBDJg48r">Gaza getiren davet</a>
           </p>
         </div>
       </div>
@@ -38,4 +38,4 @@ function home() {
   );
 }
 
-export default home;
+export default Home;
